a785bb7a3cc699ec5a866c2e4d099ffc129da44b
Sabib Ahmed <67859165+Ahmedsabib@users.noreply.github.com>
2022-04-11 23:20:37 +0600

    Create 12478 - Hardest Problem Ever (Easy).cpp

---
+++ 12478 - Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void run_case() {
+	cout << "KABIR" << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+    
+    int tests = 1;
+
+    while (tests-- > 0)
+        run_case();
+
+    return 0;
+}
